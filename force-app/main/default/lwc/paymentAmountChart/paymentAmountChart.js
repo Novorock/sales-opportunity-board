f8fc680351a076c2bef6a5a98449079ad4f36516
Novorock <pasta.chemarmazovich@mail.ru>
2023-12-26 01:44:58 +0300

    payment stat display

--- force-app/main/default/lwc/paymentAmountChart/paymentAmountChart.js
+++ force-app/main/default/lwc/paymentAmountChart/paymentAmountChart.js
@@ -1,29 +1,39 @@
 import { LightningElement, track } from 'lwc';
+import getPaymentStat from '@salesforce/apex/PaymentStatChartService.getPaymentStat';
 import chartjs from '@salesforce/resourceUrl/ChartJs';
 import { loadScript } from 'lightning/platformResourceLoader';
 
 export default class PaymentAmountChart extends LightningElement {
+    loading = true;
     @track
     isChartInitialized = false;
     chart;
+    values = [0, 100];
 
-    config = {
-        type: "doughnut",
-        data: {
-            labels: ["Paid Amount", "Remain Amount"],
-            datasets: [{
-                backgroundColor: ["#08ABED", "#7F7F7F"],
-                data: [100, 200]
-            }]
-        },
-        options: {
-            title: {
-                display: true,
-                resizeDelay: 3,
-                responsive: false,
-                text: "Opportunities Payments Stats"
+    renderChart() {
+        const config = {
+            type: "doughnut",
+            data: {
+                labels: ["Paid Amount", "Remain Amount"],
+                datasets: [{
+                    backgroundColor: ["#0070D2", "#C9C9C9"],
+                    data: this.values
+                }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    resizeDelay: 3,
+                    responsive: false,
+                    text: "Opportunities Payments Stats"
+                }
             }
         }
+
+        const ctx = this.template.querySelector('canvas.dntChart').getContext('2d');
+        this.chart = new window.Chart(ctx, config);
+        this.chart.canvas.parentNode.style.max_height = '270px';
+        this.chart.canvas.parentNode.style.max_width = '450px';
     }
 
     renderedCallback() {
@@ -35,11 +45,12 @@ export default class PaymentAmountChart extends LightningElement {
         Promise.all([
             loadScript(this, chartjs)
         ]).then(() => {
-            const ctx = this.template.querySelector('canvas.dntChart').getContext('2d');
-            let copy = JSON.parse(JSON.stringify(this.config));
-            this.chart = new window.Chart(ctx, copy);
-            this.chart.canvas.parentNode.style.max_height = '270px';
-            this.chart.canvas.parentNode.style.max_width = '450px';
+            getPaymentStat().then((data) => {
+                this.values = [];
+                this.values.push(data.PaidAmount);
+                this.values.push(data.RemainAmount);
+                this.renderChart();
+            });
         }).catch(error => {
             console.log(`Unable to load chartjs: ${error}`);
         });
