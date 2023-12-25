7df5e82106f960fc04b8602033a173a616ba899d
Novorock <pasta.chemarmazovich@mail.ru>
2023-12-25 18:44:12 +0300

    add and delete items from white list

--- force-app/main/default/lwc/accountContactListPopup/accountContactListPopup.js
+++ force-app/main/default/lwc/accountContactListPopup/accountContactListPopup.js
@@ -1,13 +1,16 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
+import getRecordsNotInWhiteList from '@salesforce/apex/WhiteListDataService.getRecordsNotInWhiteList';
 
 export default class AccountContactListPopup extends LightningElement {
     @api confirmCallback;
     @api cancelCallback;
+    loading = true;
 
     columns = [
         { label: "Name", fieldName: "Name" },
         { label: "Record Type", fieldName: "RecordType" }
     ];
+    @track
     records = [...Array(15)].map((_, index) => {
         const recordsTypes = ['Account', 'Contact'];
         return {
@@ -16,19 +19,26 @@ export default class AccountContactListPopup extends LightningElement {
             RecordType: recordsTypes[Math.floor(Math.random() * recordsTypes.length)]
         }
     });
-    selectedUsersIds = new Set([]);
+    selectedItems = [];
+
+    connectedCallback() {
+        getRecordsNotInWhiteList().then((data) => {
+            let copy = JSON.parse(JSON.stringify(data));
+            this.records = copy.items;
+            this.loading = false;
+        })
+    }
 
     handleSelection(e) {
-        let selectedRows = e.detail.selectedRows;
-        this.selectedUsersIds.clear();
-        selectedRows.forEach(el => {
-            this.selectedUsersIds.add(el.Id);
+        // eslint-disable-next-line no-unused-vars
+        this.selectedItems = e.detail.selectedRows.map((element, _) => {
+            return element.Id;
         });
     }
 
     confirm(e) {
         e.target?.blur();
-        this.confirmCallback(this.selectedUsersIds);
+        this.confirmCallback(this.selectedItems);
         this.cancelCallback();
     }
 
--- force-app/main/default/lwc/whiteList/whiteList.js
+++ force-app/main/default/lwc/whiteList/whiteList.js
@@ -1,5 +1,8 @@
 import { LightningElement, track } from 'lwc';
 import getAllWhiteListRecords from "@salesforce/apex/WhiteListDataService.getAllWhiteListItems"
+import addToWhiteList from '@salesforce/apex/WhiteListDataService.addToWhiteList';
+import removeFromWhiteList from '@salesforce/apex/WhiteListDataService.removeFromWhiteList';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class WhiteList extends LightningElement {
     loading = true;
@@ -18,24 +21,28 @@ export default class WhiteList extends LightningElement {
             RecordType: recordsTypes[Math.floor(Math.random() * recordsTypes.length)]
         }
     });
-    selectedIds = new Set();
+    selectedIds = [];
 
-    connectedCallback() {
+    refreshData() {
         getAllWhiteListRecords().then((data) => {
             let copy = JSON.parse(JSON.stringify(data));
             this.records = copy.items;
             this.loading = false;
         });
     }
-    
+
+    connectedCallback() {
+        this.refreshData();
+    }
+
     handleSelection(e) {
         let selectedRows = e.detail.selectedRows;
-        this.selectedIds.clear();
+        this.selectedIds = [];
         selectedRows.forEach(el => {
-            this.selectedIds.add(el.Id);
+            this.selectedIds.push(el.Id);
         });
-        
-        this.hint = this.selectedIds.size > 0 ? `${this.selectedIds.size} items selected` : ""; 
+
+        this.hint = this.selectedIds.size > 0 ? `${this.selectedIds.size} items selected` : "";
     }
 
     openAccountContactListPopup(e) {
@@ -48,10 +55,42 @@ export default class WhiteList extends LightningElement {
     }
 
     addRecordsToWhiteList = (records) => {
-        console.log(records);
+        if (records.length > 0) {
+            addToWhiteList({ recordsIds: records }).then(() => {
+                this.dispatchEvent(new ShowToastEvent({
+                    title: "Success!",
+                    message: "Records are added to white list",
+                    variant: "success"
+                }));
+                this.loading = true;
+                this.refreshData();
+            }).catch((error) => {
+                this.dispatchEvent(new ShowToastEvent({
+                    title: "Woops! Something went wrong",
+                    message: error.message,
+                    variant: "error"
+                }));
+            });
+        }
     }
 
     deleteRecordsFromWhiteList() {
-        console.log(this.selectedIds);
+        if (this.selectedIds.length > 0) {
+            removeFromWhiteList({ recordsIds: this.selectedIds }).then(() => {
+                this.dispatchEvent(new ShowToastEvent({
+                    title: "Success!",
+                    message: "Records are removed from white list",
+                    variant: "success"
+                }));
+                this.loading = true;
+                this.refreshData();
+            }).catch((error) => {
+                this.dispatchEvent(new ShowToastEvent({
+                    title: "Woops! Something went wrong",
+                    message: error.message,
+                    variant: "error"
+                }));
+            });
+        }
     }
 }
